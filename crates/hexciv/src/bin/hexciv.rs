5bbefc9d6589c04a6fddbe75bd92b11315ee33aa
Teoh Han Hui <teohhanhui@gmail.com>
2024-09-10 03:21:01 +0800

    Add sea ice for polar regions

--- crates/hexciv/src/bin/hexciv.rs
+++ crates/hexciv/src/bin/hexciv.rs
@@ -24,6 +24,8 @@ const ODD_ROW_OFFSET: f64 = 0.5 * GRID_SIZE.x as f64;
 const TILE_CENTER_TO_CENTER_X: f64 = GRID_SIZE.x as f64;
 const TILE_CENTER_TO_CENTER_Y: f64 = 0.8660254 * GRID_SIZE.x as f64;
 
+const TILE_LABEL_Z_INDEX: f32 = 2.0;
+
 #[derive(Copy, Clone, Eq, PartialEq)]
 #[repr(u32)]
 enum BaseTerrain {
@@ -43,11 +45,35 @@ enum BaseTerrainVariant {
     Mountains = 10,
 }
 
+#[derive(Copy, Clone)]
+#[repr(u32)]
+enum TerrainFeatures {
+    Ice = 0,
+}
+
+enum EarthLatitude {
+    ArticCirle,
+    TropicOfCancer,
+    TropicOfCapricorn,
+    AntarcticCircle,
+}
+
 #[derive(Deref, Resource)]
 struct FontHandle(Handle<Font>);
 
 #[derive(Resource)]
+struct MapSeed(u64);
+
+#[derive(Resource)]
 struct CursorPos(Vec2);
+
+/// Marker for the base terrain layer tilemap.
+#[derive(Component)]
+struct BaseTerrainLayer;
+
+/// Marker for the terrain features layer tilemap.
+#[derive(Component)]
+struct TerrainFeaturesLayer;
 
 #[derive(Component)]
 struct TileLabel(Entity);
@@ -58,10 +84,28 @@ struct HighlightedLabel;
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, SystemSet)]
 struct SpawnTilemapSet;
 
+impl EarthLatitude {
+    pub const fn latitude(&self) -> f64 {
+        match self {
+            EarthLatitude::ArticCirle => 66.57,
+            EarthLatitude::TropicOfCancer => 23.43,
+            EarthLatitude::TropicOfCapricorn => -23.43,
+            EarthLatitude::AntarcticCircle => -66.57,
+        }
+    }
+}
+
 impl FromWorld for FontHandle {
     fn from_world(world: &mut World) -> Self {
         let asset_server = world.resource::<AssetServer>();
         Self(asset_server.load("fonts/NotoSans/NotoSans-Regular.ttf"))
+    }
+}
+
+impl FromWorld for MapSeed {
+    fn from_world(_world: &mut World) -> Self {
+        let rng = fastrand::Rng::new();
+        Self(rng.get_seed())
     }
 }
 
@@ -71,6 +115,10 @@ impl Default for CursorPos {
         // correctly when the cursor moves.
         Self(Vec2::new(-1000.0, -1000.0))
     }
+}
+
+impl TerrainFeaturesLayer {
+    const Z_INDEX: f32 = 1.0;
 }
 
 fn main() {
@@ -89,6 +137,7 @@ fn main() {
         .add_plugins(PanCamPlugin)
         .add_plugins(TilemapPlugin)
         .init_resource::<FontHandle>()
+        .init_resource::<MapSeed>()
         .init_resource::<CursorPos>()
         .add_systems(
             Startup,
@@ -102,7 +151,7 @@ fn main() {
 }
 
 /// Generates the initial tilemap.
-fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>, map_seed: Res<MapSeed>) {
     commands.spawn(Camera2dBundle::default()).insert(PanCam {
         grab_buttons: vec![MouseButton::Left],
         min_scale: 1.0,
@@ -157,8 +206,8 @@ fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
     };
 
     let mut rng = fastrand::Rng::new();
-    let seed = rng.get_seed();
-    info!(seed, "map seed");
+    rng.seed(map_seed.0);
+    info!(seed = map_seed.0, "map seed");
 
     let terrain = {
         let config = fastlem_random_terrain::Config {
@@ -227,15 +276,21 @@ fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
                     -90.0 + 180.0 * ((f64::from(tile_pos.y) + 0.5) / f64::from(map_size.y));
 
                 let choice = *rng
-                    .choice(if latitude >= 66.57 || latitude <= -66.57 {
-                        &frigid_zone_tile_choices
-                    } else if latitude >= 35.0 || latitude <= -35.0 {
-                        &temperate_zone_tile_choices
-                    } else if latitude >= 23.43 || latitude <= -23.43 {
-                        &subtropics_tile_choices
-                    } else {
-                        &tropics_tile_choices
-                    })
+                    .choice(
+                        if latitude >= EarthLatitude::ArticCirle.latitude()
+                            || latitude <= EarthLatitude::AntarcticCircle.latitude()
+                        {
+                            &frigid_zone_tile_choices
+                        } else if latitude >= 35.0 || latitude <= -35.0 {
+                            &temperate_zone_tile_choices
+                        } else if latitude >= EarthLatitude::TropicOfCancer.latitude()
+                            || latitude <= EarthLatitude::TropicOfCapricorn.latitude()
+                        {
+                            &subtropics_tile_choices
+                        } else {
+                            &tropics_tile_choices
+                        },
+                    )
                     .unwrap();
 
                 TileTextureIndex(if elevation >= 25.0 {
@@ -260,42 +315,96 @@ fn spawn_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let map_type = TilemapType::Hexagon(HexCoordSystem::RowOdd);
 
-    commands.entity(tilemap_entity).insert(TilemapBundle {
-        grid_size: GRID_SIZE,
-        size: map_size,
-        storage: tile_storage,
-        texture: texture_vec,
-        tile_size: TILE_SIZE,
-        map_type,
-        transform: get_tilemap_center_transform(&map_size, &GRID_SIZE, &map_type, 0.0),
-        ..Default::default()
-    });
+    commands
+        .entity(tilemap_entity)
+        .insert(TilemapBundle {
+            grid_size: GRID_SIZE,
+            size: map_size,
+            storage: tile_storage,
+            texture: texture_vec,
+            tile_size: TILE_SIZE,
+            map_type,
+            transform: get_tilemap_center_transform(&map_size, &GRID_SIZE, &map_type, 0.0),
+            ..Default::default()
+        })
+        .insert(BaseTerrainLayer);
+
+    let image_handles = vec![asset_server.load("tiles/overlay-ice.png")];
+    let texture_vec = TilemapTexture::Vector(image_handles);
+
+    let tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+
+    commands
+        .entity(tilemap_entity)
+        .insert(TilemapBundle {
+            grid_size: GRID_SIZE,
+            size: map_size,
+            storage: tile_storage,
+            texture: texture_vec,
+            tile_size: TILE_SIZE,
+            map_type,
+            transform: get_tilemap_center_transform(&map_size, &GRID_SIZE, &map_type, 0.0)
+                * Transform::from_xyz(0.0, 0.0, TerrainFeaturesLayer::Z_INDEX),
+            ..Default::default()
+        })
+        .insert(TerrainFeaturesLayer);
 }
 
+#[allow(clippy::type_complexity)]
 fn post_spawn_tilemap(
-    tilemap_query: Query<(&TilemapSize, &TileStorage)>,
+    mut commands: Commands,
+    map_seed: Res<MapSeed>,
+    base_terrain_tilemap_query: Query<(&TilemapSize, &TileStorage), With<BaseTerrainLayer>>,
+    mut terrain_features_tilemap_query: Query<
+        (Entity, &mut TileStorage),
+        (With<TerrainFeaturesLayer>, Without<BaseTerrainLayer>),
+    >,
     mut tile_query: Query<&mut TileTextureIndex>,
 ) {
-    for (map_size, tile_storage) in tilemap_query.iter() {
-        for x in 0..map_size.x {
-            for y in 0..map_size.y {
-                let tile_pos = TilePos { x, y };
-                let tile_entity = tile_storage.get(&tile_pos).unwrap();
-                let tile_texture = tile_query.get(tile_entity).unwrap();
-                if tile_texture.0 != BaseTerrain::Ocean as u32 {
-                    continue;
-                }
-                let neighbor_entities =
-                    HexNeighbors::get_neighboring_positions_row_odd(&tile_pos, map_size)
-                        .entities(tile_storage);
-                if neighbor_entities.iter().any(|neighbor_entity| {
-                    let tile_texture = tile_query.get(*neighbor_entity).unwrap();
-                    tile_texture.0 != BaseTerrain::Ocean as u32
-                        && tile_texture.0 != BaseTerrain::Coast as u32
-                }) {
-                    let mut tile_texture = tile_query.get_mut(tile_entity).unwrap();
-                    tile_texture.0 = BaseTerrain::Coast as u32;
-                }
+    let mut rng = fastrand::Rng::new();
+    rng.seed(map_seed.0);
+
+    let ice_choices = [true, true, true, false];
+
+    let (map_size, base_terrain_tile_storage) = base_terrain_tilemap_query.get_single().unwrap();
+    let (terrain_features_tilemap_entity, mut terrain_features_tile_storage) =
+        terrain_features_tilemap_query.get_single_mut().unwrap();
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = base_terrain_tile_storage.get(&tile_pos).unwrap();
+            let tile_texture = tile_query.get(tile_entity).unwrap();
+            if tile_texture.0 != BaseTerrain::Ocean as u32 {
+                continue;
+            }
+            let neighbor_entities =
+                HexNeighbors::get_neighboring_positions_row_odd(&tile_pos, map_size)
+                    .entities(base_terrain_tile_storage);
+            if neighbor_entities.iter().any(|neighbor_entity| {
+                let tile_texture = tile_query.get(*neighbor_entity).unwrap();
+                tile_texture.0 != BaseTerrain::Ocean as u32
+                    && tile_texture.0 != BaseTerrain::Coast as u32
+            }) {
+                let mut tile_texture = tile_query.get_mut(tile_entity).unwrap();
+                tile_texture.0 = BaseTerrain::Coast as u32;
+            }
+
+            let latitude = -90.0 + 180.0 * ((f64::from(tile_pos.y) + 0.5) / f64::from(map_size.y));
+
+            if (latitude >= EarthLatitude::ArticCirle.latitude()
+                || latitude <= EarthLatitude::AntarcticCircle.latitude())
+                && rng.choice(ice_choices).unwrap()
+            {
+                let tile_entity = commands
+                    .spawn(TileBundle {
+                        position: tile_pos,
+                        tilemap_id: TilemapId(terrain_features_tilemap_entity),
+                        texture_index: TileTextureIndex(TerrainFeatures::Ice as u32),
+                        ..Default::default()
+                    })
+                    .id();
+                terrain_features_tile_storage.set(&tile_pos, tile_entity);
             }
         }
     }
@@ -304,7 +413,10 @@ fn post_spawn_tilemap(
 /// Generates tile position labels of the form: `(tile_pos.x, tile_pos.y)`
 fn spawn_tile_labels(
     mut commands: Commands,
-    tilemap_query: Query<(&Transform, &TilemapType, &TilemapGridSize, &TileStorage)>,
+    tilemap_query: Query<
+        (&Transform, &TilemapType, &TilemapGridSize, &TileStorage),
+        With<BaseTerrainLayer>,
+    >,
     tile_query: Query<&mut TilePos>,
     font_handle: Res<FontHandle>,
 ) {
@@ -314,27 +426,28 @@ fn spawn_tile_labels(
         color: Color::BLACK,
     };
     let text_justify = JustifyText::Center;
-    for (map_transform, map_type, grid_size, tilemap_storage) in tilemap_query.iter() {
-        for tile_entity in tilemap_storage.iter().flatten() {
-            let tile_pos = tile_query.get(*tile_entity).unwrap();
-            let tile_center = tile_pos.center_in_world(grid_size, map_type).extend(1.0);
-            let transform = *map_transform * Transform::from_translation(tile_center);
+    let (map_transform, map_type, grid_size, tilemap_storage) = tilemap_query.get_single().unwrap();
+    for tile_entity in tilemap_storage.iter().flatten() {
+        let tile_pos = tile_query.get(*tile_entity).unwrap();
+        let tile_center = tile_pos
+            .center_in_world(grid_size, map_type)
+            .extend(TILE_LABEL_Z_INDEX);
+        let transform = *map_transform * Transform::from_translation(tile_center);
 
-            let label_entity = commands
-                .spawn(Text2dBundle {
-                    text: Text::from_section(
-                        format!("{x}, {y}", x = tile_pos.x, y = tile_pos.y),
-                        text_style.clone(),
-                    )
-                    .with_justify(text_justify),
-                    transform,
-                    ..Default::default()
-                })
-                .id();
-            commands
-                .entity(*tile_entity)
-                .insert(TileLabel(label_entity));
-        }
+        let label_entity = commands
+            .spawn(Text2dBundle {
+                text: Text::from_section(
+                    format!("{x}, {y}", x = tile_pos.x, y = tile_pos.y),
+                    text_style.clone(),
+                )
+                .with_justify(text_justify),
+                transform,
+                ..Default::default()
+            })
+            .id();
+        commands
+            .entity(*tile_entity)
+            .insert(TileLabel(label_entity));
     }
 }
 
@@ -360,13 +473,16 @@ fn update_cursor_pos(
 fn highlight_tile_labels(
     mut commands: Commands,
     cursor_pos: Res<CursorPos>,
-    tilemap_query: Query<(
-        &TilemapSize,
-        &TilemapGridSize,
-        &TilemapType,
-        &TileStorage,
-        &Transform,
-    )>,
+    tilemap_query: Query<
+        (
+            &TilemapSize,
+            &TilemapGridSize,
+            &TilemapType,
+            &TileStorage,
+            &Transform,
+        ),
+        With<BaseTerrainLayer>,
+    >,
     highlighted_tiles_query: Query<Entity, With<HighlightedLabel>>,
     tile_label_query: Query<&TileLabel>,
     mut text_query: Query<&mut Text>,
@@ -385,31 +501,31 @@ fn highlight_tile_labels(
         }
     }
 
-    for (map_size, grid_size, map_type, tile_storage, map_transform) in tilemap_query.iter() {
-        // Grab the cursor position from the `Res<CursorPos>`
-        let cursor_pos: Vec2 = cursor_pos.0;
-        // We need to make sure that the cursor's world position is correct relative to
-        // the map due to any map transformation.
-        let cursor_in_map_pos: Vec2 = {
-            // Extend the cursor_pos vec3 by 0.0 and 1.0
-            let cursor_pos = Vec4::from((cursor_pos, 0.0, 1.0));
-            let cursor_in_map_pos = map_transform.compute_matrix().inverse() * cursor_pos;
-            cursor_in_map_pos.xy()
-        };
-        // Once we have a world position we can transform it into a possible tile
-        // position.
-        if let Some(tile_pos) =
-            TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
-        {
-            // Highlight the relevant tile's label
-            if let Some(tile_entity) = tile_storage.get(&tile_pos) {
-                if let Ok(label) = tile_label_query.get(tile_entity) {
-                    if let Ok(mut tile_text) = text_query.get_mut(label.0) {
-                        for section in tile_text.sections.iter_mut() {
-                            section.style.color = palettes::tailwind::RED_600.into();
-                        }
-                        commands.entity(tile_entity).insert(HighlightedLabel);
+    let (map_size, grid_size, map_type, tile_storage, map_transform) =
+        tilemap_query.get_single().unwrap();
+    // Grab the cursor position from the `Res<CursorPos>`
+    let cursor_pos: Vec2 = cursor_pos.0;
+    // We need to make sure that the cursor's world position is correct relative to
+    // the map due to any map transformation.
+    let cursor_in_map_pos: Vec2 = {
+        // Extend the cursor_pos vec3 by 0.0 and 1.0
+        let cursor_pos = Vec4::from((cursor_pos, 0.0, 1.0));
+        let cursor_in_map_pos = map_transform.compute_matrix().inverse() * cursor_pos;
+        cursor_in_map_pos.xy()
+    };
+    // Once we have a world position we can transform it into a possible tile
+    // position.
+    if let Some(tile_pos) =
+        TilePos::from_world_pos(&cursor_in_map_pos, map_size, grid_size, map_type)
+    {
+        // Highlight the relevant tile's label
+        if let Some(tile_entity) = tile_storage.get(&tile_pos) {
+            if let Ok(label) = tile_label_query.get(tile_entity) {
+                if let Ok(mut tile_text) = text_query.get_mut(label.0) {
+                    for section in tile_text.sections.iter_mut() {
+                        section.style.color = palettes::tailwind::RED_600.into();
                     }
+                    commands.entity(tile_entity).insert(HighlightedLabel);
                 }
             }
         }
